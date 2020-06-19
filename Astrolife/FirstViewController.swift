b87f158d76ca181b8deae3ae1780f4cfccbee4df
selimerhan <seliwerhau@gmail.com>
2020-06-19 15:32:15 +0300

    Initial Commit

---
+++ Astrolife/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Astrolife
+//
+//  Created by Selim Erhan on 6/19/20.
+//  Copyright © 2020 Selim Erhan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Astrolife/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Astrolife
+//
+//  Created by Selim Erhan on 6/19/20.
+//  Copyright © 2020 Selim Erhan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
